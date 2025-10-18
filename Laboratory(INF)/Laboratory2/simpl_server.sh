e24f67c83be245d8e975f3b8b213b4893d969338
Kaaai69 <guereiro150@gmail.com>
2025-10-18 10:34:42 +0300

    news

--- Laboratory(INF)/Laboratory2/simpl_server.sh
+++ Laboratory(INF)/Laboratory2/simpl_server.sh
@@ -4,7 +4,7 @@ echo "Start server"
 
 while true; do
     echo "Waiting for messages"
-    nc -l -p 12345 | while read message; do
+    nc -l 12345 | while read message; do
 	echo "Receive message: $message"
 	echo "Had message" | nc localhost 12345
     done
